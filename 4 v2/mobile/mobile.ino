7bd2bbcc6fe804c937453a461fc111f874cfb2d0
smekhnyov <156292095+smekhnyov@users.noreply.github.com>
2024-06-30 13:27:30 +0300

    Add 1st module
    Co-authored-by: nice.sevkabr@mail.ru <nice.sevkabr@mail.ru>

--- 4 v2/mobile/mobile.ino
+++ 4 v2/mobile/mobile.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
 #include <Keypad.h>
 
 #define LCD_RS 8
@@ -32,12 +33,113 @@ Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 void setup() {
   lcd.begin(16,2);
+  Serial.begin(9600);
+}
+
+void enter_password(int password)
+{
+  char key;
+  int try_count = 0;
+  int try_password = 0;
+  bool enter_flag = false;
+  while (!enter_flag)
+  {
+    key = keypad.getKey();
+    if (key != NO_KEY)
+    {
+      if (key != '#' && key != '*')
+      {
+        lcd.print(key);
+        if (try_password == 0)
+        {
+          try_password = key - '0';
+        }
+        else
+        {
+          try_password = try_password*10 + (key - '0');
+        }
+      }
+      else if (key == '#')
+      {
+        try_count++;
+        Serial.print(try_password);
+        delay(500);
+        if (Serial.available() > 0)
+        {
+          char answer = Serial.read();
+          if (answer == 'D')
+          {
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print("Correct!");
+            enter_flag = true;
+          }
+          else if (answer == 'E')
+          {
+            try_password = 0;
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print("Incorrect!");
+            lcd.setCursor(0,1);
+            lcd.print("Popitok: ");
+            lcd.print(3 - try_count);
+            delay(1000);
+            lcd.clear();
+            lcd.setCursor(0,0);
+            lcd.print(password);
+            lcd.setCursor(0,1);
+          }
+        }
+      }
+      else if (key == '*')
+      {
+        try_password = 0;
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print(password);
+        lcd.setCursor(0,1);
+      }
+    }
+    if (try_count > 2)
+    {
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("Popitok nema!");
+      enter_flag = true;
+    }
+  }
+}
+
+void receive_password()
+{
+  bool received_responce = false;
+  while (!received_responce)
+  {
+    if (Serial.available() > 0)
+    {
+      int password = Serial.parseInt();
+      received_responce = true;
+      Serial.print('C');
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print(password);
+      lcd.setCursor(0,1);
+      enter_password(password);
+    }
+  }
 }
 
 void loop() {
-  char key = keypad.getKey();
-
-  if (key) {
-    lcd.print(key);
+  if (Serial.available() > 0) 
+  {  
+    char command = Serial.read();
+    switch (command)
+    {
+      case 'A':
+        Serial.print('B');
+        lcd.print("Initialize...");
+        delay(500);
+        receive_password();
+    }
   }
 }
--- 4 v2/station/station.ino
+++ 4 v2/station/station.ino
@@ -1,4 +1,6 @@
 #include <GyverStepper.h>
+#include <SoftwareSerial.h>
+#include <time.h>
 
 #define LED_1_R 15
 #define LED_1_Y 14
@@ -26,38 +28,109 @@
 
 #define SPEAKER 9
 
+bool tumbler_1_flag = false;
+bool tumbler_2_flag = false;
+bool tumbler_3_flag = false;
+
 GStepper<STEPPER2WIRE> stepper(32, STEPPER_NB, STEPPER_PB, STEPPER_NA, STEPPER_PA);
 
-
 void setup() {
-  // pinMode(LED_1_R, OUTPUT);
-  // pinMode(LED_1_Y, OUTPUT);
-  // pinMode(LED_1_G, OUTPUT);
-  // pinMode(LED_2_R, OUTPUT);
-  // pinMode(LED_2_Y, OUTPUT);
-  // pinMode(LED_2_G, OUTPUT);
-  // pinMode(LED_3_R, OUTPUT);
-  // pinMode(LED_3_Y, OUTPUT);
-  // pinMode(LED_3_G, OUTPUT);
+  pinMode(TUMBLER_1, INPUT);
+  pinMode(TUMBLER_2, INPUT);
+  pinMode(TUMBLER_3, INPUT);
 
-  stepper.setRunMode(FOLLOW_POS);
-  stepper.setSpeed(10);
-  stepper.setTarget(-32);
+  pinMode(SPEAKER, OUTPUT);
+
+  pinMode(ENCODER_1_DT, INPUT_PULLUP);
+  pinMode(ENCODER_1_CLK, INPUT_PULLUP);
+  pinMode(ENCODER_2_DT, INPUT_PULLUP);
+  pinMode(ENCODER_2_CLK, INPUT_PULLUP);
+
+  stepper.setSpeed(300);
+
+  pinMode(LED_1_R, OUTPUT);
+  pinMode(LED_1_Y, OUTPUT);
+  pinMode(LED_1_G, OUTPUT);
+  pinMode(LED_2_R, OUTPUT);
+  pinMode(LED_2_Y, OUTPUT);
+  pinMode(LED_2_G, OUTPUT);
+  pinMode(LED_3_R, OUTPUT);
+  pinMode(LED_3_Y, OUTPUT);
+  pinMode(LED_3_G, OUTPUT);
+
+  Serial.begin(9600);
+
+  digitalWrite(LED_1_R, HIGH);
+  digitalWrite(LED_2_R, HIGH);
+  digitalWrite(LED_3_R, HIGH);
+
+  srand(time(NULL));
+
+}
+
+int generate_random_key() {
+  return rand()%9999;
+}
+
+void enter_key() {
+  int key = generate_random_key();
+  bool flag = false;
+  Serial.print(key);
+  while (!flag) {
+    if (Serial.available() > 0) 
+    {  
+      char command = Serial.read();
+      if (command == 'C') {
+        bool flag2 = false;
+        while (!flag2) {
+          if (Serial.available() > 0)
+          {
+            int password = Serial.parseInt();
+            if (password == key) {
+              Serial.print('D');
+              flag = true;
+              flag2 = true;
+              digitalWrite(LED_1_Y, LOW);
+              digitalWrite(LED_1_G, HIGH);
+            }
+            else {
+              Serial.print('E');
+              digitalWrite(SPEAKER, HIGH);
+              delay(100);
+              digitalWrite(SPEAKER, LOW);
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+void mobile_connect() {
+  bool flag = false;
+  while (!flag) {
+    if (Serial.available() > 0) 
+    {  
+      char command = Serial.read();
+      if (command == 'B') {
+        digitalWrite(LED_1_R, LOW);
+        digitalWrite(LED_1_Y, HIGH);
+        enter_key();
+        flag = true;
+      }
+    }
+  }
+}
+void wait_1st_tumbler() {
+  while (!digitalRead(TUMBLER_1)) {
+    tumbler_1_flag = true;
+    Serial.print('A');
+    mobile_connect();
+  }
 }
 
 void loop() {
-  // digitalWrite(LED_1_R, HIGH);
-  // digitalWrite(LED_1_Y, HIGH);
-  // digitalWrite(LED_1_G, HIGH);
-  // digitalWrite(LED_2_R, HIGH);
-  // digitalWrite(LED_2_Y, HIGH);
-  // digitalWrite(LED_2_G, HIGH);
-  // digitalWrite(LED_3_R, HIGH);
-  // digitalWrite(LED_3_Y, HIGH);
-  // digitalWrite(LED_3_G, HIGH);
-  if (!stepper.tick()) {
-    static bool dir;
-    dir = !dir;
-    stepper.setTarget(dir ? -1024 : 1024);
-  }
+  wait_1st_tumbler();
+  
+  // stepper.runSpeed();
 }
